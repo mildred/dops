eda16e0ed4006683b344e18b4045d4d53069162e
Shanti Bouchez <shanti@sogilis.com>
2013-01-09 09:45:41 +0100

    cipush

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -29,7 +29,7 @@ while true; do
 done
 
 HEAD="$(git rev-parse HEAD)"
-git commit $COMMIT_OPTIONS -m "Auto-generated commit for provisionning"
+git commit --allow-empty $COMMIT_OPTIONS -m "Auto-generated commit for provisionning"
 echo "HEAD is now $(git rev-parse HEAD)"
 (set -x; git -c push.default=simple push -f "$@")
 git reset --soft "$HEAD"
